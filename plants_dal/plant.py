d0d135538036efde5ab39f7fcc17b7d02e9ab784
Sebastian Valenzuela <sebastiandfg@gmail.com>
2012-12-18 16:46:44 -0300

    fix response to search

--- plants_dal/plant.py
+++ plants_dal/plant.py
@@ -17,7 +17,7 @@ class Plant(object):
     @staticmethod
     def _db_to_plant(plant_result):
         if plant_result:
-            plant = {'mame':plant_result[0],'photo':plant_result[1],'wiki':plant_result[2],'descriptors':plant_result[3]}
+            plant = {'name':plant_result[0],'photo':plant_result[1],'wiki':plant_result[2],'descriptors':plant_result[3]}
             if len(plant_result)>4:
                 plant['distance'] = plant_result[4]
             return plant
@@ -27,10 +27,10 @@ class Plant(object):
     @staticmethod
     def retrieve(db, name):
         db.execute("SELECT name,photo,wiki,descriptors FROM Leaves WHERE name=%s LIMIT 1",(name,))
-        plant = db.fetchone()
-        if plant:
-            return Plant._db_to_plant(plant)
-        return None
+        plants = []
+        for plant in db.fetchall():
+            plants.append(Plant._db_to_plant(plant))
+        return plants
 
 
     @staticmethod
@@ -38,5 +38,8 @@ class Plant(object):
         db.execute('''SELECT name, photo, wiki, descriptors, vdistance(descriptors ,cast(%s as double precision[])) as distance
                       FROM Leaves
                       ORDER BY distance ASC
-                      LIMIT 1''', (descriptors,))
-        return Plant._db_to_plant(db.fetchone())
+                      LIMIT 2''', (descriptors,))
+        plants = []
+        for plant in db.fetchall():
+            plants.append(Plant._db_to_plant(plant))
+        return plants
